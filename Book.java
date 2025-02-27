fb82afbb38c4ba7c7604e8b54aaebe27c11e7c69
sobhan-25 <mondalspike0987654321@gmail.com>
2025-02-27 17:20:04 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,1 +1,37 @@
+public class Book {
+    private String title;
+    private String author;
+    private String ISBN;
+    private String genre;
+    private boolean available;
 
+    // Constructor
+    public Book(String title, String author, String ISBN, String genre, boolean available) {
+        this.title = title;
+        this.author = author;
+        this.ISBN = ISBN;
+        this.genre = genre;
+        this.available = available;
+    }
+
+    // Getters and Setters
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getISBN() { return ISBN; }
+    public void setISBN(String ISBN) { this.ISBN = ISBN; }
+
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public boolean isAvailable() { return available; }
+    public void setAvailable(boolean available) { this.available = available; }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + "\nAuthor: " + author + "\nISBN: " + ISBN + "\nGenre: " + genre + "\nAvailable: " + (available ? "Yes" : "No");
+    }
+}
